33cbf83193bb15b0359682e203036a8066b0682c
drcz <1406282+drcz@users.noreply.github.com>
2024-02-12 01:35:44 +0100

    desperate attempts at CPS conversion, early prototype (wip)

--- cps.scm
+++ cps.scm
@@ -1,31 +1,10 @@
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;; -= a prototype for CPS-conversion of pindolf programs =-
-;;; so that the VM could perform (GOTO 0) instead of (CALL ...)
-;;; it's not even a good idea to do that, let alone do it this way!
-
+;;; it's not even a good idea! \o/
 (use-modules (grand scheme))
 (add-to-load-path "./")
 (define parsed (@ (pindolf parser) parsed))
 (define pindolf (@ (pindolf interpreter) pindolf))
+;;; the rest of stuff we'll just copy'n'paste since we alter it a bit
 
-(define test1
-  '(
-    (('fold-r ?op ?e    ()     ) e)
-    (('fold-r ?op ?e (?x . ?xs)) (& (,op ,x ,(& (fold-r ,op ,e ,xs)))))
-
-    (('cons ?h ?t) `(,h . ,t))
-    (('apd ?xs ?ys) (& (fold-r cons ,ys ,xs)))
-
-    ((('cons*f.hd ?f) ?h ?t) (& (cons ,(& (,f ,h)) ,t)))
-    (('map ?f ?xs) (& (fold-r (cons*f.hd ,f) () ,xs)))
-
-    (('dup ?x) `(,x . ,x))
-    (('dbl %n) (+ n n))
-
-    (('rev ?xs) (& (rev ,xs ())))
-    (('rev    ()      ?rs) rs)
-    (('rev (?x . ?xs) ?rs) (& (rev ,xs (,x . ,rs))))
-    ))
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;;; ok so we'd like something like
@@ -63,8 +42,7 @@
   (('hK %x %y ?k) (& (fK ,x (hK* ,y ,k))))
   ((('hK* %y ?k) ?res) (& (fK ,y (hK** ,res ,k))))
   ((('hK** ?res ?k) ?res*) (& (,k (+ ,res ,res*)))))
-;;; the structure-building operations are invisible.
-
+;;; since ,,the structure-building operations are invisible''
 (e.g. 
  (pindolf '(hK 2 3 id)
           '((('fK %x ?k) (& (,k ,(* x x))))
@@ -78,11 +56,6 @@
 ;;; only produce `num?` test in the decision tree (FCL*). it's redundant
 ;;; since the types can't go wrong on the generated continuations...
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;; ok some ugly conventions to get it running ; we might append 'K'
-;;; to the names, and use stars Peano-style... ooor we could
-;;; just convert `label` to `(label 0)`, `(label 1)`
-;;; and similarly with res being (res 0) (res 1) etc. cool?
 
 ;;; ah! one more thing, quite disturbing: what if the patterns do not
 ;;; stick to the ,,prefix convention'', e.g. we could have (%m '+ %n)
@@ -90,10 +63,10 @@
 ;;; since this is only a prototype we can ignore it for now, but
 ;;; if we ever seriously need to automagically convert to CPS, it'd
 ;;; better use some more abstract representation... we'll get there
-;;; but indeed, after all pindol(f) is more low-level than languages
+;;; but indeed, after all pindol[f] is more low-level than languages
 ;;; with procedures, therefore CPS is a foreign idea to it.
 
-;;; another nasty assumption: k is not bound in any pattern.
+;;; a nasty assumption: k is not bound in any pattern
 ;;; one more reason to start thinking about abstract representation...
 
 (define (cps-pattern p kont) ;;; ,,for now''...
@@ -121,7 +94,7 @@
 
 ;;; so it actually should consume clause and return n+1 ones, where n
 ;;; is the number of applications in the expression.
-;;; borrowing from the compiler then:
+;;; borrowing from the compiler then (primop? used tho):
 
 (define (applications-in expression)
   (define (applications-in-qq qq)
@@ -148,4 +121,249 @@
 ;;; so then kont for A0 must enclose m and its kont k, while
 ;;; kont for A1 must enclose A0's result r0 and k, and then the outermost
 ;;; expression would be applying k to `(+ ,r0 ,m). now, how do we keep
-;;; track of the things which should get enclosed???
+;;; track of the things which should get enclosed??? they should fall
+;;; into one the three categories only:
+;;; (1) surely the continuation of the outermost clause, since we need
+;;;     to eventually apply it.
+;;; (2) result(s) of earlier application(s), since we need to use them,
+;;;     but then stop passing them once they're used?
+;;; (3) every variable boud by the outermost clause but not yet used.
+;;; well, (1) is trivial but for (2) and (3) another example would help:
+'( (('f ?x) (* x x))
+   (('g ?x) (+ x 1))
+   (('h ?x ?y) (+ (& (f ,x)) (& (f ,(& (g ,y)))))) )
+;;; soo
+(e.g. (applications-in '(+ (& (f ,x)) (& (f ,(& (g ,y))))))
+      ===> (#;A0 (& (f ,x))
+            #;A1 (& (g ,y))
+            #;A2 (& (f ,(& (g ,y))))) )
+;;; which would turn into something like
+'((res0 . (& (f ,x))) ;; keep y and k
+  (res1 . (& (g ,y))) ;; x no longer needed, BUT keep res0 and k
+  (res2 . (& (f ,res1)))) ;;; y and res1 no longer needed, keep res0 and k
+;;; with outermost (+ res0 res2), right. one more hand-written CPS:
+'( (('fK ?x ?k) (& (,k ,(* x x))))
+   (('gK ?x ?k) (& (,k ,(+ x 1))))
+   (('hK ?x ?y ?k) (& (fK ,x (hK* ,y ,k))))
+   ((('hK* ?y ?k) ?res0) (& (gK ,y (hK** ,res0 ,k))))
+   ((('hK** ?res0 ?k) ?res1) (& (fK ,res1 (hK*** ,res0 ,k))))
+   ((('hK*** ?res0 ?k) ?res2) (& (,k ,(+ res0 res2)))) )
+;;; like that?
+(e.g.
+ (pindolf '(hK 2 3 id)
+          '( (('fK ?x ?k) (& (,k ,(* x x))))
+             (('gK ?x ?k) (& (,k ,(+ x 1))))
+             (('hK ?x ?y ?k) (& (fK ,x (hK* ,y ,k))))
+             ((('hK* ?y ?k) ?res0) (& (gK ,y (hK** ,res0 ,k))))
+             ((('hK** ?res0 ?k) ?res1) (& (fK ,res1 (hK*** ,res0 ,k))))
+             ((('hK*** ?res0 ?k) ?res2) (& (,k ,(+ res0 res2))))
+             (('id ?x) x) )) ===> 20)
+;;; ok. it all kind of makes sense, but how did i know that all?
+;;; what do i have to see to figure out what's needed?
+;;; or MAYBE we'll be fine keeping it all enclosed and just hoping
+;;; that the partial evaluator will get rid of dead subexpressions?
+;;; like this:
+;;; res0: keep x,y,k
+;;; res1: keep x,y,k,res0
+;;; res2: keep x,y,k,res0, res1 ??? 
+(e.g.
+ (pindolf '(hK 2 3 id)
+          '( (('fK ?x ?k) (& (,k ,(* x x))))
+             (('gK ?x ?k) (& (,k ,(+ x 1))))
+             (('hK ?x ?y ?k)
+              #;--> (& (fK ,x (hK* ,x ,y ,k))))
+             ((('hK* ?x ?y ?k) ?res0)
+              #;--> (& (gK ,y (hK** ,x ,y ,k ,res0))))
+             ((('hK** ?x ?y ?k ?res0) ?res1)
+              #;--> (& (fK ,res1 (hK*** ,x ,y ,k ,res0 ,res1))))
+             ((('hK*** ?x ?y ?k ?res0 ?res1) ?res2)
+              #;--> (& (,k ,(+ res0 res2))))
+             (('id ?x) x) )) ===> 20)
+;;; hmmm, well at least that works and feels manageable.
+
+;;; just for now all types will be ? (exp) and we won't check for
+;;; unique names of generated labels m'kay? it's going to be a mess
+;;; but just to figure out what it takes... let's do this!
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; i'll repaste from the above just-in-case...
+
+(define member? member) ;; he_he
+(define (primop? x) (member? x '(+ - *)))
+
+(define (applications-in expression)
+  (define (applications-in-qq qq)
+    (match qq
+      (('unquote e) (applications-in e))
+      ((e . e*) (append (applications-in-qq e) (applications-in-qq e*)))
+      (_ '())))
+  (match expression
+    (() '())
+    ('T '())
+    ((? symbol?) '())
+    ((? number?) '())
+    (('quote _) '())
+    (('quasiquote qq) (applications-in-qq qq))
+    (((? primop?) e e*) (append (applications-in e) (applications-in e*)))
+    (('& e) (append (applications-in-qq e) `(,expression)))))
+
+;;; surely also:
+(define (lookup key #;in binding)
+  (match binding
+    (() #f)
+    (((k . expr) . binding*) (if (equal? k key) expr
+                              #;otherwise (lookup key binding*)))))
+
+;;; now borrowing from parser (dropped massage):
+(define (vartype? x) (member? x '(num sym atm exp)))
+
+(define (vars-in-pattern p)
+  (match p
+    (((? vartype?) v) `(,v))
+    (('quote _) '())
+    ((p . ps) (union (vars-in-pattern p) (vars-in-pattern ps)))
+    (_ '())))
+
+
+;;; yeah let's make it pretty since otherwise it can blow the interpreter
+(define (mk-label (l id))
+  (string->symbol (string-append (symbol->string l)
+                                 (number->string id))))
+(e.g. (mk-label '(res 0)) ===> res0)
+(e.g. (mk-label '(KONT 5)) ===> KONT5)
+
+;;; ...and from compiler again (applications injected and 'res thing):
+(define (application->res-map applications)
+  (map (lambda (app index) `(,app . ,(mk-label `(res ,index))))
+       applications (iota (length applications))))
+
+;;; aaand now! replacing applications with res-vars INSIDE applications:
+(define (reduced-subexpression expr #;wrt res-for-apps)
+  (define (reduced-qq qq)
+    (match qq
+      (() '())
+      (('unquote e) ;; nb mindfuck ahead
+       `(,'unquote ,(reduced-subexpression e res-for-apps)))
+      ((q . q*) `(,(reduced-qq q) . ,(reduced-qq q*)))
+      (e e))) ;; sure?
+  (let rdcd ((expr expr))
+    (match expr
+      (() '())
+      ('T 'T)
+      ((? symbol? sym) sym)
+      ((? number? num) num)
+      (('quote e) expr)
+      (('quasiquote qq) (reduced-qq qq))
+      (((? primop? p) e e*) `(,p ,(rdcd e) ,(rdcd e*)))
+      (('& e) (or (lookup expr res-for-apps) ;;; !!!!!!
+                  `(& ,(reduced-qq e))))
+      #;err??)))
+
+(e.g. (let* ((expr '(+ (& (f ,x)) (& (f ,(& (g ,y))))))
+             (apps (applications-in expr))
+             (res4apps (application->res-map apps)))        
+        (reduced-subexpression expr res4apps))
+      ===> (+ res0 res2))
+;;; only we'll have to build new res4apps for consecutive members
+;;; of apps with just ones above it, and in the end the source expr
+;;; like above. got it.
+
+(e.g. (let* ((expr '(+ (& (f ,x)) (& (f ,(& (g ,y))))))
+             (apps (applications-in expr))
+             (res4apps (application->res-map apps)))
+        (reduced-subexpression (third apps) (take res4apps 2)))
+      ===> (& (f ,res1)))
+;;; yeeesssss!
+
+(define (expression2steps expression)
+  (let* ((applications (applications-in expression))
+         (res4apps (application->res-map applications)))
+    (let loop ((steps '())
+               (todo `(,@applications ,expression)))
+    (match todo
+      (() steps)
+      ((e . todo*) (let* ((index (length steps)) ;; :D
+                          (res4as (take res4apps index))
+                          (my-res (mk-label `(res ,index))) ;;; he_he
+                          (my-kont (mk-label `(KONT ,index)))
+                          (steps* `(,@steps
+                                    (,my-res
+                                     ,my-kont
+                                    ,(reduced-subexpression e res4as)))))
+                     (loop steps* todo*)))))))
+
+(e.g. (expression2steps '(+ (& (f ,x)) (& (f ,(& (g ,y))))))
+      ===> ((res0 KONT0 (& (f ,x)))
+            (res1 KONT1 (& (g ,y)))
+            (res2 KONT2 (& (f ,res1)))
+            (res3 KONT3 (+ res0 res2))))
+;;; and this should turn into:
+;; * <massaged pattern> (& (f ,x (KONT0 <pattern vars w/ k>))) 
+;; * (('KONT0 <pvwk>) ?res0) (& (g ,y (KONT1 <pvwk> ,res0)))
+;; * (('KONT1 <pvwk> ?res0) ?res1) (& (f ,res1 (KONT2 <pv> ,res0 ,res1)))
+;; * (('KONT2 <pv> ?res0 ?res1) ?res2) (& (,k ,(+ res0 res1)))
+
+(define (cps-step e kont)
+  (match e
+    (('& a) `(& (,@a ,kont)))
+    (e `(& (,kont (,'unquote ,e))))))
+
+(e.g. (cps-step '(& (f ,x)) 'k)
+      ===> (& (f ,x k)))
+(e.g. (cps-step '(+ (res 0) (res 2)) 'k)
+      ===> (& (k ,(+ (res 0) (res 2)))))
+
+(define (cps-pattern p kont) ;;; ,,for now''...
+  `(,@p (exp ,kont)))
+
+(e.g. (cps-pattern '('h (exp x) (exp y)) 'k)
+      ===> ('h (exp x) (exp y) (exp k)))
+
+(define (vars2pat vs) (map (lambda (v) `(exp ,v)) vs))
+  (e.g. (vars2pat '(x y k res0 res1))
+        ===> ((exp x) (exp y) (exp k) (exp res0) (exp res1)))
+
+(define (vars2exp vs) (map (lambda (v) `(,'unquote ,v)) vs))
+  (e.g. (vars2exp '(x y k res0 res1))
+        ===> (,x ,y ,k ,res0 ,res1))
+
+;;; looks quite ok? so now...
+
+(define (cps clause kont) ;; ensure its massaged (parsed)!
+  (let* (((pattern expression) clause)
+         (vars (vars-in-pattern pattern))
+         (pattern* (cps-pattern pattern kont))
+         (steps (expression2steps expression))
+         (last-kont kont))
+    (let loop ((todo steps)
+               (done '())
+               (pat pattern*)
+               (keeping `(,@vars ,kont)))
+      (match todo
+        (() done)
+        (((res kont step) . todo*)
+         (let* ((kont* (if (= 0 (length todo*))
+                           last-kont
+                           `(,kont ,@(vars2exp keeping))))
+                (step* (cps-step step kont*))
+                (pat* `((',kont ,@(vars2pat keeping)) (exp ,res)))
+                (keeping* `(,@keeping ,res))
+                (done* `(,@done (,pat ,step*))))
+           (loop todo* done* pat* keeping*)))))))
+
+(e.g.
+ (equal?
+  (cps '(('h (exp x) (exp y)) (+ (& (f ,x)) (& (f ,(& (g ,y)))))) 'K)
+  (parsed
+   '( (('h ?x ?y ?K)
+       #;--> (& (f ,x (KONT0 ,y ,x ,K))))
+      ((('KONT0 ?y ?x ?K) ?res0)
+       #;--> (& (g ,y (KONT1 ,y ,x ,K ,res0))))
+      ((('KONT1 ?y ?x ?K ?res0) ?res1)
+       #;--> (& (f ,res1 (KONT2 ,y ,x ,K ,res0 ,res1))))
+      ((('KONT2 ?y ?x ?K ?res0 ?res1) ?res2)
+       #;--> (& (K ,(+ res0 res2)))) ))))
+
+;;; wooooooooow
+
+        
